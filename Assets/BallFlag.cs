1223d01e644a862073b890ece1301963ca4960b5
Hugo Peters <hpeters1024@gmail.com>
2017-03-30 23:12:28 +0200

    Added shitty blocks in the middle

---
+++ Assets/BallFlag.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFlag : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.color = new Color(205 / 255f, 83 / 255f, 239 / 255f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -11,12 +11,15 @@ public class Spawner : MonoBehaviour {
     public Platform prefab;
     public PlayerControls player;
     public AbstractEnemy[] enemies;
+    public BallFlag ball;
     public int sightRange;
     int count;
+    int holeCounter;
     float offset;
 	void Start () {
         count = 0;
         offset = 0;
+        holeCounter = 0;
 	}
 	
 	// Update is called once per frame
@@ -25,31 +28,52 @@ public class Spawner : MonoBehaviour {
 		while (player.transform.position.x > (count-sightRange))
         {
             offset += Random.Range(0, 2f)-1f;
-            for (int i = 0; i < (count < 40 ? 20 : levelWidth); ++i)
+            if (count % 25 == 0)
             {
-                Platform lastSpawn = Instantiate(prefab);
-                lastSpawn.gameObject.name = "BasePlatform";
-                lastSpawn.Player = player;
-                lastSpawn.transform.localPosition += new Vector3(count -2, 0, offset + i);
-                int z = Random.Range(0, 10);
-                switch(z)
+                BallFlag b = Instantiate(ball);
+                b.transform.localPosition += new Vector3(count - 2, 1, offset);
+            }
+            if (Random.Range(0, 100) == 0)
+            {
+                holeCounter = Random.Range(1, 4);
+            }
+
+            if (holeCounter-- <= 0)
+            {
+                for (int i = 0; i < (count < 40 ? 20 : levelWidth); ++i)
                 {
-                    case 0:
-                        PhysicMaterial m = new PhysicMaterial();
-                        m.bounciness = 1;
-                        lastSpawn.GetComponent<BoxCollider>().material = m;
-                        lastSpawn.GetComponent<Renderer>().material.color = Color.red;
-                        lastSpawn.Kind = 1;
-                        break;
+                    Platform lastSpawn = Instantiate(prefab);
+                    lastSpawn.gameObject.name = "BasePlatform";
+                    lastSpawn.Player = player;
+                    lastSpawn.transform.localPosition += new Vector3(count - 2, 0, offset + i);
+                    int z = Random.Range(0, 10);
+                    switch (z)
+                    {
+                        case 0:
+                            PhysicMaterial m = new PhysicMaterial();
+                            m.bounciness = 1;
+                            lastSpawn.GetComponent<BoxCollider>().material = m;
+                            lastSpawn.GetComponent<Renderer>().material.color = Color.red;
+                            lastSpawn.Kind = 1;
+                            break;
 
-                    case 1:
-                        lastSpawn.GetComponent<Renderer>().material.color = Color.blue;
-                        lastSpawn.Kind = 2;
-                        break;
+                        case 1:
+                            lastSpawn.GetComponent<Renderer>().material.color = Color.blue;
+                            lastSpawn.Kind = 2;
+                            break;
 
-                    default:
-                        lastSpawn.GetComponent<Renderer>().material.color = new Color(22 / 255f, 119 / 255f, 58 /255f);
-                        break;
+                        default:
+                            lastSpawn.GetComponent<Renderer>().material.color = new Color(22 / 255f, 119 / 255f, 58 / 255f);
+                            break;
+                    }
+                }
+                if (Random.Range(0, 30) == 0)
+                {
+                    Platform lastSpawn = Instantiate(prefab);
+                    lastSpawn.gameObject.name = "BasePlatform";
+                    lastSpawn.Player = player;
+                    lastSpawn.transform.localPosition += new Vector3(count - 2, 0.5f, offset + Random.Range(0,levelWidth));
+                    lastSpawn.transform.localScale = new Vector3(1, 0.5f, 1);
                 }
             }
 
